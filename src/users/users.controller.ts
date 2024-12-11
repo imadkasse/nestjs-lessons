955384a43838ea9fee471506d1ffa31188f51e9e
imad kasse <kasseimad81@gmail.com>
2024-12-11 16:54:35 +0100

    lesson-03

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,30 +9,42 @@ import {
   Patch,
   Post,
   Query,
+  Req,
   Res,
 } from '@nestjs/common';
-import { Response } from 'express';
+import { Request, Response } from 'express';
+import { UsersService } from './users.service';
 
 @Controller('users') // هذا يشكل الراوت => http://localhost:3000/users
 export class UsersController {
+  constructor(private readonly userService: UsersService) {}
   @Get() // type the method(GET, POST, PUT, PATCH,DELETE) , you can add route in THE METHOD HTTP => ex: GET('/users/info')
-  findAllUsers(@Res() res: Response) {
+  async findAllUsers(@Res() res: Response) {
     res.status(HttpStatus.OK).json({
       message: 'Get all users',
-      users: ['John Doe', 'Jane Doe'],
+      users: await this.userService.findAllUsers(),
     });
   }
   //you can add parameters like this
   @Get(':id/')
   @HttpCode(201) // to add status code
-  findOne(@Param('id') id: string) {
-    return `User with ID: ${id}`;
+  findOne(@Param('id') id: string, @Res() res: Response) {
+    const user = this.userService.findOne(id);
+
+    res.status(HttpStatus.OK).json({
+      message: 'Get one user',
+      user,
+    });
   }
 
   @Post('/create')
   // Body to get request body
-  createUser(@Body() body) {
-    return `User created with data: ${JSON.stringify(body)}`;
+  async createUser(@Body() body, @Req() req: Request, @Res() res: Response) {
+    const newUser = await this.userService.create(req.body);
+    res.status(HttpStatus.CREATED).json({
+      message: 'User created successfully',
+      newUser,
+    });
   }
   @Patch('/update/:id')
   updateUser(@Param('id') id: string, @Body() body, @Res() res: Response) {
