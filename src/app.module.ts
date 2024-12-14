52ee71286d2d3610a6baf9fa0d95704f25ac1568
imadkasse <kasseimad81@gmail.com>
2024-12-14 12:19:40 +0100

    first step ins middelware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { LoggerMiddleware } from './logger/logger.middleware';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, UsersController],
   providers: [AppService],
 })
-export class AppModule {}
+// call the middleware 
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('users');
+  }
+}
